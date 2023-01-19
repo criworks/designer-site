161e9a0e36659a569c7406969e9e3807e6f0ddf4
Cri <crstphx@gmail.com>
2023-01-18 21:55:25 -0300

    [fix] todo el setup necesario para sass y svg

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/main.css'
+import '../styles/main.sass'
+// Poder importar directamente sass es gracias a Next y su built-in sass que se configura en next.config
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
